4fe0adb64975e06aa504c61ed2eabecbf5de670c
Ionică Bizău <bizauionica@yahoo.com>
2013-12-10 18:11:54 +0200

    Pass template field.

--- test/1.js
+++ test/1.js
@@ -1,7 +1,8 @@
 var Crud = require("../operations");
 
 Crud.read({
-    query: {}
+    query: {},
+    t: "528f78dc197ef1c23714a04d"
 }, function (err, data) {
     console.log(err, data);
     process.exit();
